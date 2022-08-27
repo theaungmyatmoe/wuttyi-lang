bbe4a00c8954f15074c92a772a0ff4ee7363177a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-08-27 20:29:44 +0630

    test: add inheritance

--- __tests__/class-test.js
+++ __tests__/class-test.js
@@ -17,4 +17,23 @@ export default function (wuttyi) {
     
      )
     `, 30)
+
+    test(wuttyi, `
+    (begin
+    
+    (class Point3D Point
+      (begin
+        (def constructor (self x y z)
+          (begin
+            ((prop (super Point3D) constructor) self x y)
+            (set (prop self z) z)))
+        (def calc (self)
+          (+ ((prop (super Point3D) calc) self)
+             (prop self z)))))
+    (var p (new Point3D 10 20 30))
+    ((prop p calc) p)
+    
+    )
+    
+    `, 60)
 }
